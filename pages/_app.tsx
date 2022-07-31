ed14d26755a60c35f2954fec6e8f1f0ee1360503
Elias Misael Valdez Faria <evaldez@cerouno.com.py>
2022-07-30 21:22:21 -0400

    ApolloProvider config added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
 import React, { PropsWithChildren } from "react";
 import { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client'
 import { UserProvider } from '@auth0/nextjs-auth0';
-import Layout from '../components/Layout';
+import client from '@graphql/client';
+import Layout from '@components/Layout';
 
 const MyApp: React.FC<PropsWithChildren<AppProps>> = ({ Component, pageProps }) => {
   return (
-    <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </UserProvider>
-
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UserProvider>
+    </ApolloProvider>
   )
 }
-
 
 export default MyApp;
